27fa7d6e344f7213c64ef65997e994034cd8e58e
Eugene Datsky <eugene@datsky.ru>
2017-02-09 18:37:01 +0300

    :lipstick:

--- index.js
+++ index.js
@@ -10,135 +10,134 @@ const matchImport = /^([\w-]+)(?:\s+as\s+([\w-]+))?/;
 
 const PLUGIN = 'postcss-modules-values-replace';
 const INNER_PLUGIN = 'postcss-modules-values-replace-bind';
-const walkerPlugin = postcss.plugin(INNER_PLUGIN, (fn, context) => fn.bind(null, context));
 
 // Borrowed from enhanced-resolve
 const nodeFs = new CachedInputFileSystem(new NodeJsInputFileSystem(), 4000);
 const concordContext = {};
+
+const getDefinition = (atRule, existingDefinitions, requiredDefinitions) => {
+  let matches;
+  const definition = {};
+
+  // eslint-disable-next-line no-cond-assign
+  while (matches = matchValueDefinition.exec(atRule.params)) {
+    const [/* match*/, key, middle, value, end] = matches;
+    const requiredName = key;
+    // Add to the definitions, knowing that values can refer to each other
+    definition[requiredName] = replaceAll(existingDefinitions, value);
+    if (!requiredDefinitions) {
+      // eslint-disable-next-line no-param-reassign
+      atRule.params = key + middle + definition[requiredName] + end;
+    }
+  }
+
+  return definition;
+};
+
+const getImport = (matches, existingDefinitions) => {
+  const imports = {};
+  // eslint-disable-next-line prefer-const
+  let [/* match*/, aliases, pathString] = matches;
+
+  // We can use constants for path names
+  if (existingDefinitions[pathString]) {
+    pathString = existingDefinitions[pathString];
+  }
+
+  // Do nothing if path is not found
+  if (!pathString.match(/"[^"]*"|'[^']*'/)) {
+    return null;
+  }
+
+  aliases.replace(/^\(\s*([\s\S]+)\s*\)$/, '$1').split(/\s*,\s*/).forEach((alias) => {
+    const tokens = matchImport.exec(alias);
+
+    if (tokens) {
+      const [/* match*/, theirName, myName = theirName] = tokens;
+      const exportsPath = pathString.replace(/['"]/g, '');
+
+      if (!imports[exportsPath]) {
+        imports[exportsPath] = {};
+      }
+
+      imports[exportsPath][theirName] = myName;
+    } else {
+      throw new Error(`@value statement "${alias}" is invalid!`);
+    }
+  });
+
+  return imports;
+};
+
+const walk = async (requiredDefinitions, walkFile, root, result) => {
+  const rules = [];
+  const fromDir = result.opts.from && path.dirname(result.opts.from);
+
+  root.walkAtRules('value', (atRule) => {
+    rules.push(atRule);
+  });
+
+  const reduceRules = async (definitionsPromise, atRule) => {
+    const existingDefinitions = await definitionsPromise;
+    const matches = matchImports.exec(atRule.params);
+
+    if (matches) {
+      const imports = getImport(matches, existingDefinitions);
+      const files = imports && Object.keys(imports);
+
+      if (!files || !files[0]) {
+        return {};
+      }
+
+
+      const definitions = await walkFile(files[0], fromDir, imports[files[0]]);
+      return Object.assign(existingDefinitions, definitions);
+    }
+
+    if (atRule.params.indexOf('@value') !== -1) {
+      result.warn(`Invalid value definition: ${atRule.params}`);
+    }
+    const newDefinitions = getDefinition(atRule, existingDefinitions, requiredDefinitions);
+
+    return Object.assign(existingDefinitions, newDefinitions);
+  };
+
+  const definitions = await rules.reduce(reduceRules, Promise.resolve({}));
+
+  if (requiredDefinitions) {
+    const validDefinitions = {};
+    Object.keys(requiredDefinitions).forEach((key) => {
+      validDefinitions[requiredDefinitions[key]] = definitions[key];
+    });
+
+    result.messages.push({
+      type: INNER_PLUGIN,
+      value: validDefinitions,
+    });
+
+    return undefined;
+  }
+
+  return definitions;
+};
+
+const walkerPlugin = postcss.plugin(INNER_PLUGIN, (fn, ...args) => fn.bind(null, ...args));
 
 const factory = ({ fs = nodeFs, resolve: options = {} } = {}) => async (root, rootResult) => {
   const resolver = ResolverFactory.createResolver(Object.assign({ fileSystem: fs }, options));
   const resolve = promisify(resolver.resolve, resolver);
   const readFile = promisify(fs.readFile, fs);
 
-  async function walkFile(from, dir, context) {
+  async function walkFile(from, dir, requiredDefinitions) {
     const resolvedFrom = await resolve(concordContext, dir, from);
     const content = await readFile(resolvedFrom);
-    // eslint-disable-next-line no-use-before-define
-    const result = await postcss([walkerPlugin(walk, context)])
+    const result = await postcss([walkerPlugin(walk, requiredDefinitions, walkFile)])
       .process(content, { from: resolvedFrom });
 
     return result.messages[0].value;
   }
 
-  const getDefinition = (atRule, existingDefinitions, requiredDefinitions) => {
-    let matches;
-    const definition = {};
-
-    // eslint-disable-next-line no-cond-assign
-    while (matches = matchValueDefinition.exec(atRule.params)) {
-      const [/* match*/, key, middle, value, end] = matches;
-      const requiredName = key;
-      // Add to the definitions, knowing that values can refer to each other
-      definition[requiredName] = replaceAll(existingDefinitions, value);
-      if (!requiredDefinitions) {
-        // eslint-disable-next-line no-param-reassign
-        atRule.params = key + middle + definition[requiredName] + end;
-      }
-    }
-
-    return definition;
-  };
-
-  const getImport = (matches, existingDefinitions) => {
-    const imports = {};
-    // eslint-disable-next-line prefer-const
-    let [/* match*/, aliases, pathString] = matches;
-
-    // We can use constants for path names
-    if (existingDefinitions[pathString]) {
-      pathString = existingDefinitions[pathString];
-    }
-
-    // Do nothing if path is not found
-    if (!pathString.match(/"[^"]*"|'[^']*'/)) {
-      return null;
-    }
-
-    aliases.replace(/^\(\s*([\s\S]+)\s*\)$/, '$1').split(/\s*,\s*/).forEach((alias) => {
-      const tokens = matchImport.exec(alias);
-
-      if (tokens) {
-        const [/* match*/, theirName, myName = theirName] = tokens;
-        const exportsPath = pathString.replace(/['"]/g, '');
-
-        if (!imports[exportsPath]) {
-          imports[exportsPath] = {};
-        }
-
-        imports[exportsPath][theirName] = myName;
-      } else {
-        throw new Error(`@value statement "${alias}" is invalid!`);
-      }
-    });
-
-    return imports;
-  };
-
-  const walk = async (requiredDefinitions, fromRoot, result) => {
-    const rules = [];
-    const fromDir = result.opts.from && path.dirname(result.opts.from);
-
-    fromRoot.walkAtRules('value', (atRule) => {
-      rules.push(atRule);
-    });
-
-    const reduceRules = async (definitionsPromise, atRule) => {
-      const existingDefinitions = await definitionsPromise;
-      const matches = matchImports.exec(atRule.params);
-
-      if (matches) {
-        const imports = getImport(matches, existingDefinitions);
-        const files = imports && Object.keys(imports);
-
-        if (!files || !files[0]) {
-          return {};
-        }
-
-
-        const definitions = await walkFile(files[0], fromDir, imports[files[0]]);
-        return Object.assign(existingDefinitions, definitions);
-      }
-
-      if (atRule.params.indexOf('@value') !== -1) {
-        result.warn(`Invalid value definition: ${atRule.params}`);
-      }
-      const newDefinitions = getDefinition(atRule, existingDefinitions, requiredDefinitions);
-
-      return Object.assign(existingDefinitions, newDefinitions);
-    };
-
-    const definitions = await rules.reduce(reduceRules, Promise.resolve({}));
-
-    if (requiredDefinitions) {
-      const validDefinitions = {};
-      Object.keys(requiredDefinitions).forEach((key) => {
-        validDefinitions[requiredDefinitions[key]] = definitions[key];
-      });
-
-      result.messages.push({
-        type: INNER_PLUGIN,
-        value: validDefinitions,
-      });
-
-      return undefined;
-    }
-
-    return definitions;
-  };
-
-
-  const definitions = await walk(null, root, rootResult);
+  const definitions = await walk(null, walkFile, root, rootResult);
   rootResult.messages.push({
     plugin: PLUGIN,
     type: 'values',
