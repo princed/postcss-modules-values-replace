442da71e2e03804855f48f305b40f71bc6e29c35
toby <toby@canva.com>
2021-05-09 01:10:49 +1000

    Cache

--- index.js
+++ index.js
@@ -100,9 +100,13 @@ const walk = async (requiredDefinitions, walkFile, root, result) => {
 
       const exportsPath = pathString.replace(/['"]/g, '');
       const imports = getImports(aliases);
-
       const definitions = await walkFile(exportsPath, fromDir, imports);
-      return Object.assign(existingDefinitions, definitions);
+
+      Object.keys(imports).forEach((key) => {
+        existingDefinitions[imports[key]] = definitions[key];
+      });
+
+      return existingDefinitions;
     }
 
     if (atRule.params.indexOf('@value') !== -1) {
@@ -116,14 +120,9 @@ const walk = async (requiredDefinitions, walkFile, root, result) => {
   const definitions = await rules.reduce(reduceRules, Promise.resolve({}));
 
   if (requiredDefinitions) {
-    const validDefinitions = {};
-    Object.keys(requiredDefinitions).forEach((key) => {
-      validDefinitions[requiredDefinitions[key]] = definitions[key];
-    });
-
     result.messages.push({
       type: INNER_PLUGIN,
-      value: validDefinitions,
+      value: definitions,
     });
 
     return undefined;
@@ -142,6 +141,7 @@ const factory = ({
   importsAsModuleRequests = false,
   replaceInSelectors = false,
 } = {}) => async (root, rootResult) => {
+  const cache = new Map();
   const resolver = ResolverFactory.createResolver(Object.assign(
     { fileSystem: fs },
     resolveOptions,
@@ -160,6 +160,12 @@ const factory = ({
   async function walkFile(from, dir, requiredDefinitions) {
     const request = importsAsModuleRequests ? urlToRequest(from) : from;
     const resolvedFrom = await resolve(concordContext, dir, request);
+
+    const cached = cache.get(resolvedFrom);
+    if (cached) {
+      return cached;
+    }
+
     const content = await readFile(resolvedFrom);
     const plugins = [
       ...preprocessPlugins,
@@ -167,6 +173,8 @@ const factory = ({
     ];
     const result = await postcss(plugins)
       .process(content, { from: resolvedFrom });
+
+    cache.set(resolvedFrom, result.messages[0].value);
 
     return result.messages[0].value;
   }
